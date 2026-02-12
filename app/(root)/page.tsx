865c2d013674a44e3f2d66fb648684edaeb8b485
Tarek Bari <baritarek4@gmail.com>
2026-02-12 02:39:13 +0100

    New Banner Version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,51 +1,11 @@
 // import Banner from "@/components/banner/Banner";
-import BannerSwiper from "@/components/carousel/bannerSwiper/bannerSwiper";
+import Banner from "@/components/carousel/rootCarousel/banner/banner";
 import styles from "./page.module.scss";
-
-// const images = ["/images/iphone1.jpg", "/images/iphone2.jpg"];
-
-const bannerSlides = [
-  {
-    img: "/images/daniel-romero-q-RQba-XCgU-unsplash.jpg",
-    title: "iPhone 14 Pro",
-    desc: "Pro. Beyond.",
-    pagination: {
-      category: "Shop the latest iPhone",
-    },
-  },
-  {
-    img: "/images/dennis-brendel-YLNMXzXk8zs-unsplash.jpg",
-    title: "iPhone 14 Plus",
-    desc: "Big and bigger.",
-    pagination: {
-      category: "Shop the latest iPhone",
-    },
-  },
-  {
-    img: "/images/img2.jpg",
-    title: "iPhone 14",
-    desc: "Wonderfull.",
-    pagination: {
-      category: "Shop the latest iPhone",
-    },
-  },
-
-  {
-    img: "/images/iphone2.jpg",
-    title: "iPhone 14",
-    desc: "Wonderfull.",
-    pagination: {
-      category: "Shop the latest iPhone",
-    },
-  },
-];
 
 const Home = () => {
   return (
     <>
-      <div className={`${styles.wrapper}`}>
-        <BannerSwiper slides={bannerSlides} />
-      </div>
+      <Banner />
       <div className={`${styles.wrapper}`}>
         <h1>after</h1>
       </div>
---
+++ components/carousel/rootCarousel/banner/banner.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { Navigation, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type { Swiper as SwiperType } from "swiper";
+import { bannerSlides } from "@/constants/rootBannerData";
+import styles from "./banner.module.scss";
+
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import "swiper/css";
+
+interface BannerSlide {
+  img: string;
+  subtitle: string;
+  body: string;
+  title: string;
+  lightDesc: string;
+  categoryButton: string;
+  pagination: {
+    category: string;
+  };
+}
+
+const Banner = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const swiperRef = useRef<SwiperType | null>(null);
+
+  return (
+    <div className={styles.banner} dir="rtl">
+      <div className={styles.bannerSwiper}>
+        <Swiper
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          onSlideChange={(swiper) => {
+            setActiveIndex(swiper.activeIndex);
+          }}
+          modules={[Navigation, Autoplay]}
+          // grabCursor
+          initialSlide={0}
+          slidesPerView={1}
+          navigation
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: true,
+          }}
+          className={styles.swiper}
+        >
+          {(bannerSlides as BannerSlide[]).map((slide, index) => (
+            <SwiperSlide key={index} className={styles.swiperSlide}>
+              <div className={styles.content}>
+                <article className={styles.contentCard} dir="rtl">
+                  <p className={styles.subtitle}>{slide.subtitle}</p>
+                  <h2 className={styles.title}>{slide.title}</h2>
+                  <p className={styles.body}>{slide.body}</p>
+                  <p className={styles.lightDesc}>{slide.lightDesc}</p>
+                  <button type="button" className={styles.categoryButton}>
+                    {slide.categoryButton}
+                  </button>
+                </article>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={slide.img}
+                  alt="Banner Image"
+                  width={1920}
+                  height={1080}
+                  priority
+                  className={styles.image}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className={styles.pagination}>
+          <div className={styles.paginationInner}>
+            {bannerSlides.map((slide, index) => (
+              <span
+                key={index}
+                className={`${index === activeIndex ? styles.active : ""} ${styles.paginationItem}`}
+                onClick={() => swiperRef.current?.slideTo(index)}
+              >
+                {slide.pagination.category}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
